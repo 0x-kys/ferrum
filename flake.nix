e1f50dffa1bed4cf02167b09cfa0ab8a291a9154
for { kys() } <yyeet62425@gmail.com>
2024-12-08 19:16:30 +0530

    nix -> flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Ferrum Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs }: 
+    let
+      system = "x86_64-linux"; # Adjust according to your system 
+      pkgs = import nixpkgs {
+        inherit system;
+        # config = { allowUnfree = true; }; # If you need unfree packages
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.rustfmt
+          pkgs.clippy
+        ];
+
+        shellHook = ''
+          echo "Welcome to the Ferrum development environment!"
+        '';
+      };
+    };
+}
